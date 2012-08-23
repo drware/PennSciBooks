968c5d00309c78110245507c92cf7bf8655cdbc2
Dave Ware <drware10@gmail.com>
2012-08-23 15:31:42 -0400

    corrected template design

--- psbooks-template.php
+++ psbooks-template.php
@@ -1,26 +1,28 @@
 <?php
 /*
-Template Name: PS Books
+* @package: WordPress
+* @subpackage: PennSci
+* @since 2012 v1.0
 */
 ?>
 <?php get_header(); ?>
 
 <div id="contentTemplate">
-
-<?php while ( have_posts() ) : the_post(); ?>
+<?php global $post; ?>
 <?php $custom = get_post_custom($post->ID); ?>
 
-
-<h1><?php the_title(); ?></h1>
-<img src="<?php echo $custom["psbooks-cover"]; ?>" width="300" height="453" align="right" />
+<div id="book" style="width:600px; margin: auto;">
+<h2><?php the_title(); ?></h2>
+<img src="<?php echo $custom["psbooks-cover"][0]; ?>" width="240" height="362" align="right" />
 <ul>
-	<?php if($custom["psbooks-preface"]) { ?>
-	<li><a href="<?php echo $custom["psbooks-preface"]; ?>" target="_blank">Preface</a></li>
+	<?php if($custom["psbooks-preface"][0]) { ?>
+	<li><a href="<?php echo $custom["psbooks-preface"][0]; ?>" target="_blank">Preface</a></li>
 	<?php } ?>
-	<?php if($custom["psbooks-toc"]) { ?>
-	<li><a href="<?php echo $custom["psbooks-toc"]; ?>" target="_blank">Table of Contents</a></li>
+	<?php if($custom["psbooks-toc"][0]) { ?>
+	<li><a href="<?php echo $custom["psbooks-toc"][0]; ?>" target="_blank">Table of Contents</a></li>
 	<?php } ?>
 </ul>
+</div>
 
 </div><div id="delimiter"></div>
 
