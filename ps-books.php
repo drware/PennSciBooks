e63ad96dca63dbc5772b1aaa23a52ef8b9545ea3
Dave Ware <drware10@gmail.com>
2012-08-25 11:42:38 -0400

    added documentation, finalized template

--- ps-books.php
+++ ps-books.php
@@ -3,18 +3,18 @@
 Plugin Name: PennSci Books
 Plugin URI: none
 Description: Allows for book listings
-Author: David Ware (drw48@pitt.edu)
-Version: 1.0
+Author: David Ware
+Version: 1.1
 Author URI: http://www.davidrware.com/
 */
 
-// Include the helper functions
-//include_once("ps_functions.php");
-
 if(!class_exists("PSBooks")) {
 	class PSBooks {
-		var $meta_fields = array("psbooks-price","psbooks-preface","psbooks-toc","psbooks-cover");		
-		// Definition of custom post type
+		// Fields specific to this content type
+		// Book price, link to preface, link to table of contents, link to cover image
+		var $meta_fields = array("psbooks-price","psbooks-preface","psbooks-toc","psbooks-cover");
+		
+		// Registers custom content type and associated actions
 		function __construct() {
 			register_post_type('book',
 			array('label'=>_x('Books','books label'),
@@ -28,42 +28,33 @@ if(!class_exists("PSBooks")) {
 				'rewrite' => array("slug" => "book"),
 				'query_var' => "book",
 				'supports' => array("title")));
-			//add_filter("manage_edit-books_columns",array(&$this,"edit_columns"));
-			//add_action("manage_posts_custom_column",array(&$this,"custom_columns"))
+				
 			add_action("admin_init", array(&$this, "admin_init"));
 			add_action("template_redirect", array(&$this,'template_redirect'));
 			add_action("wp_insert_post", array(&$this,"wp_insert_post"),10,2);
 		}		
-		//function edit_columns($columns) {
-		//	$columns = array(
-		//	'cb' => '<input type="checkbox" />',
-		//	'title' => _x("Book Title","book title label for edit columns")
-		//	);
-		//}		
-		//function custom_columns($column) {
-		//	global $post;
-			//switch($column) {
-			//	case "psbooks_title"
-		//}		
+		// Assigning the default template
 		function template_redirect() {
 			global $wp;
-			
 			if(isset($wp->query_vars["post_type"])) {
 				if($wp->query_vars["post_type"] == "book") {
+					// File is included in plugin dir
 					include WP_PLUGIN_DIR.'/psbooks/psbooks-template.php';
 					die();
 				}
 			}
 		}
+		// Defining how to insert and update entries of this type
 		function wp_insert_post($post_id, $post = null) {
 			if($post->post_type == "book") {
 				foreach ($this->meta_fields as $key) {
 					$value = $_POST[$key];
+					// Empties meta fields
 					if(empty($value)) {
 						delete_post_meta($post_id, $key);
 						continue;
 					}
-
+					// Updates and adds meta field values
 					if(!is_array($value)) {
 						if(!update_post_meta($post_id, $key, $value)) {
 							add_post_meta($post_id, $key, $value);
@@ -77,9 +68,11 @@ if(!class_exists("PSBooks")) {
 				}
 			}
 		}
+		// Adds entry to admin menu
 		function admin_init() {
 			add_meta_box("psbook-meta","Book Information",array(&$this,"meta_options"),"book","normal","core");
 		}
+		// Template for content entry in dashboard
 		function meta_options() {
 			global $post;
 			$custom = get_post_custom($post->ID);
@@ -100,18 +93,19 @@ if(!class_exists("PSBooks")) {
 				$psbooks_cover = $custom["psbooks-cover"][0];
 			} else { $psbooks_cover = ''; }
 			?>
-			<label><?php _ex('Price:','price label'); ?></label><input name="psbooks-price" value="<?php echo $psbooks_price; ?>" /><br />
-			<em><?php _ex('(e.g. $39.99)','pricing example'); ?></em><br /><br />			
+			<label><?php _ex('Price:','price label'); ?></label><input name="psbooks-price" value="$<?php echo $psbooks_price; ?>" /><br />
+			<em><?php _ex('(e.g. 39.99)','pricing example'); ?></em><br /><br />			
 			<label><?php _ex('Preface URL:','preface URL label'); ?></label><input name="psbooks-preface" value="<?php echo $psbooks_preface; ?>" /><br />
-			<em><?php _ex('(e.g. /wp-content/uploads/book-preface.pdf)','preface URL example'); ?></em><br /><br />			
+			<em><?php _ex('(e.g. book-preface.pdf)','preface URL example'); ?></em><br /><br />			
 			<label><?php _ex('Table of Contents URL:','ToC label'); ?></label><input name="psbooks-toc" value="<?php echo $psbooks_toc; ?>" /><br />
-			<em><?php _ex('(e.g. /wp-content/uploads/book-toc.pdf)','ToC URL example'); ?></em><br /><br />			
+			<em><?php _ex('(e.g. book-toc.pdf)','ToC URL example'); ?></em><br /><br />			
 			<label><?php _ex('Cover URL:','cover URL label'); ?></label><input name="psbooks-cover" value="<?php echo $psbooks_cover; ?>" /><br />
-			<em><?php _ex('(e.g. /wp-content/uploads/book-cover.jpg)','cover URL example'); ?></em><br /><br />
+			<em><?php _ex('(e.g. book-cover.jpg)','cover URL example'); ?></em><br /><br />
 			<?php
 		}
 	}
 }
+// Initiates plugin
 if(class_exists('PSBooks')) {
 	//Initiate the plugin
 	add_action("init","PSBookInit");	
--- psbooks-template.php
+++ psbooks-template.php
@@ -13,13 +13,13 @@
 
 <div id="book" style="width:600px; margin: auto;">
 <h2><?php the_title(); ?></h2>
-<img src="<?php echo $custom["psbooks-cover"][0]; ?>" width="240" height="362" align="right" />
+<img src="/wp-content/uploads/<?php echo $custom["psbooks-cover"][0]; ?>" width="240" height="362" align="right" />
 <ul>
 	<?php if($custom["psbooks-preface"][0]) { ?>
-	<li><a href="<?php echo $custom["psbooks-preface"][0]; ?>" target="_blank">Preface</a></li>
+	<li><a href="/wp-content/uploads/<?php echo $custom["psbooks-preface"][0]; ?>" target="_blank">Preface</a></li>
 	<?php } ?>
 	<?php if($custom["psbooks-toc"][0]) { ?>
-	<li><a href="<?php echo $custom["psbooks-toc"][0]; ?>" target="_blank">Table of Contents</a></li>
+	<li><a href="/wp-content/uploads/<?php echo $custom["psbooks-toc"][0]; ?>" target="_blank">Table of Contents</a></li>
 	<?php } ?>
 </ul>
 </div>
